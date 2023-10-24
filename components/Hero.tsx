a9d74fae6b0e237b442a65ebe28c6ba4fe76b458
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-10-24 11:12:26 -0500

    added logic

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,6 +23,11 @@ const Hero = () => {
         handleClick={handleScroll}
         />
       </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image />
+        </div>
+      </div>
     </div>
   )
 }
