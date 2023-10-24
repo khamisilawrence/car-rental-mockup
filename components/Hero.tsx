e2b9f04e0d61d80eb2aec19b42b62ac9f523dee4
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-10-24 10:59:49 -0500

    added logic

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from "@/node_modules/next/image";
+import CustomButton from "./CustomButton";
+
 
 const Hero = () => {
+  const handleScroll = () => {}
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -11,6 +16,12 @@ const Hero = () => {
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking process.
         </p>
+
+        <CustomButton 
+        title="Explore Cars"
+        containerStyles="bg-primary-blue text-white rounded-full mt-10"
+        handleClick={handleScroll}
+        />
       </div>
     </div>
   )
