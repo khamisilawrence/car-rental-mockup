299f0843164605945ab38fecd5cb7c484373f7f6
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-11-11 09:01:18 -0500

    added logic

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -4,12 +4,23 @@ import { Fragment, useState } from "react";
 import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
 import { SearchManufacturerProps } from "@/types";
+import { manufacturers } from "@/constants";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
   const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
   return (
     <div className="search-manufacturer">
