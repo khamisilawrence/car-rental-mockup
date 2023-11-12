870fe17efd6b5cf31dd66315fffa328cd4075775
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-11-12 09:18:52 -0500

    imported CustomButton component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import { CarProps } from "@/types";
+import { CustomButton } from ".";
 
 interface CarCardProps {
   car: CarProps;
