3f64a5fe72bf6af5ebaf57a929108f9e3cb28d95
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-11-12 09:28:35 -0500

    updated logic, formatting, styling, & content

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -12,7 +12,19 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
-  return <div>CarCard</div>;
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p>
+        <span>Car Rent...</span>
+      </p>
+    </div>
+  );
 };
 
 export default CarCard;
