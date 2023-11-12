d8e1fa472fe418f06c97c4e61cd54a49d793066a
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-11-12 09:40:03 -0500

    removed unnecessary logic

--- utils/index.ts
+++ utils/index.ts
@@ -1,22 +1,5 @@
 import { json } from "stream/consumers";
 
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '3a2c9d1d46msh7e2a2b35220aac4p163f29jsnde7f818c35ec',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-
 export async function fetchCars() {
   const headers = {
 		'X-RapidAPI-Key': '3a2c9d1d46msh7e2a2b35220aac4p163f29jsnde7f818c35ec',
