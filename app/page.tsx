b1b3ea927d7a7820a31f088915bfe2d74e685300
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-11-15 13:24:27 -0500

    added logic: passed props to ShowMore component

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,10 @@ export default async function Home({ searchParams }: { searchParams: any }) {
                 <CarCard car={car} />
               ))}
             </div>
-            <ShowMore />
+            <ShowMore
+              pageNumber={(searchParams.pageNumber || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
