6966baf0c519a52a753af5976b219bfa7fb685b1
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-11-15 10:53:21 -0500

    imported necessary dependencies

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
 
 const CustomFilter = () => {
   return <div></div>;
